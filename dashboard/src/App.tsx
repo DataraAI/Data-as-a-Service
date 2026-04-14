f74e700a4afa90a3c270e894349d8096d3959bd5
tanay924 <73369719+tanay924@users.noreply.github.com>
2026-04-14 16:46:13 +0100

    Add files via upload

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import DataViewer from "./pages/DataViewer";
 import ExploreDatasets from "./pages/ExploreDatasets";
+import RoboEyeView from "./pages/RoboEyeView";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/explore" element={<ExploreDatasets />} />
+      <Route path="/roboeyeview" element={<RoboEyeView />} />
       <Route path="/viewer/*" element={<DataViewer />} />
     </Routes>
   </BrowserRouter>
