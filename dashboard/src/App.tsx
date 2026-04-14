41019e87b7714485e650c8aba8faab450642b5e4
Aayush Dubey <aayush@dataraai.ai>
2026-04-14 15:43:19 +0000

    Revert "Add files via upload"
    This reverts commit bf052ecb2a9f9f45eb92f7e52b8dfa9382de1b45.

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,14 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import DataViewer from "./pages/DataViewer";
 import ExploreDatasets from "./pages/ExploreDatasets";
-import RoboEyeView from "./pages/RoboEyeView";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/explore" element={<ExploreDatasets />} />
-      <Route path="/roboeyeview" element={<RoboEyeView />} />
       <Route path="/viewer/*" element={<DataViewer />} />
     </Routes>
   </BrowserRouter>
