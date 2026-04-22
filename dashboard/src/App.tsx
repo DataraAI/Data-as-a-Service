6ee461948d97942474cd1f3487ca1becefbb6f9b
tanay924 <73369719+tanay924@users.noreply.github.com>
2026-04-22 01:44:05 +0100

    Add files via upload

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import DataViewer from "./pages/DataViewer";
 import RoboEyeView from "./pages/RoboEyeView";
+import AuthPage from "./pages/AuthPage";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<AuthPage />} />
       <Route path="/product" element={<Product />} />
       <Route path="/explore" element={<ExploreDatasets />} />
       <Route path="/roboeyeview" element={<RoboEyeView />} />
