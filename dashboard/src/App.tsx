f7285b36a5bd1b8dea52795dfa7762fa95cb2024
tanay924 <73369719+tanay924@users.noreply.github.com>
2026-04-26 01:51:40 +0100

    Add files via upload

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -4,20 +4,20 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import DataViewer from "./pages/DataViewer";
 import RoboEyeView from "./pages/RoboEyeView";
-import AuthPage from "./pages/AuthPage";
-import AdminUsers from "./pages/AdminUsers";
+import { Toaster } from "./components/ui/sonner";
 
 const App = () => (
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/admin/users" element={<AdminUsers />} />
-      <Route path="/product" element={<Product />} />
-      <Route path="/explore" element={<ExploreDatasets />} />
-      <Route path="/roboeyeview" element={<RoboEyeView />} />
-      <Route path="/viewer/*" element={<DataViewer />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/explore" element={<ExploreDatasets />} />
+        <Route path="/roboeyeview" element={<RoboEyeView />} />
+        <Route path="/viewer/*" element={<DataViewer />} />
+      </Routes>
+      <Toaster richColors />
+    </>
   </BrowserRouter>
 );
 
