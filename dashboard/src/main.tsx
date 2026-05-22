568a47102aba919eb86d24e1778b1c9eef57113f
tanay924 <73369719+tanay924@users.noreply.github.com>
2026-05-22 20:40:07 +0100

    Add files via upload

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,13 +1,27 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider, useTheme } from "next-themes";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
 
+function ThemeInitializer() {
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    setTheme("light");
+  }, [setTheme]);
+
+  return null;
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} storageKey="datara-theme">
+      <ThemeInitializer />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
